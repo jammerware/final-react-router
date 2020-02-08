448caf5a22eb4e07b7186cd92f09b77d77943b68
Glitch (starter-react-template) <none>
2020-02-08 02:38:49 +0000

    🙇🍳 Checkpoint ./src/tweets-service.js:10218203/3225 ./src/App.js:10218203/1629

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import YouTube from 'react-youtube';
+import { TweetsService } from './tweets-service';
 
 class App extends Component {
   constructor(state) {
@@ -12,30 +13,36 @@ class App extends Component {
   
   componentDidMount() {
     // setInterval(() => this.setState({ theDate: new Date() }), 1000);
+    this.loadTweets();
+  }
+  
+  loadTweets() {
+    const service = new TweetsService();
+    this.setState({ trending: service.getTrending() });
   }
   
   render() {
+    const trendingElements = []; 
+    for (let i = 0; i < this.state.trending; i++) {
+      const trend = this.state.trending[i];
+      
+      trendingElements.push(
+        <li>
+          <h2>{trend.text}</h2>
+          <p>{trend.type}</p>
+        </li>
+      );
+    }
     
     return (
       <div className="app">
         <header className="App-header">
-          <h1>Hi, INFSCI 2560!</h1>
-          <img src={logo} className="app-logo" alt="logo" />
+          <h1>Trending on Twitter</h1>
         </header>
 
         <section className="content">
-          {this.state.theDate && <p>It's currently {this.state.theDate.toString()}.</p>}
-          <p>Here are my favorite things about you, as a class:</p>
-          <ul>
-            <li>You're brilliant!</li>
-            <li>
-              You ask&nbsp;
-              <a href="https://www.google.com/search?q=why+do+we+drive+on+parkways+and+park+on+driveways&oq=why+do+we+drive+on+parkways+&aqs=chrome.0.0j69i57j0l5.3070j0j7&sourceid=chrome&ie=UTF-8">great questions</a>
-            </li>
-            <li>You're awake and paying rapt attention (... right?)</li>
-          </ul>
-          
-          <YouTube videoId="iTvBy_TjUR4" width="100%"></YouTube>
+          <ul className="trending-list">{trendingElements}</ul>
+
         </section>
       </div>
     );
--- src/tweets-service.js
+++ src/tweets-service.js
@@ -1,5 +1,19 @@
-function getTrending() {
-  
+class TweetsService {
+  getTrending() {
+    const trending = [
+      { text: "#FishAreFriendsNotFood", type: "hashtag" },
+      { text: "#MarmotsArePeopleToo", type: "hashtag" },
+      { text: "#JustInfSciThings", type: "hashtag" },
+      { text: "Colonel Vindman", type: "topic" },
+      { text: "My Brother, My Brother, and Me", type: "topic" },
+      { text: "Jeopardy!", type: "topic" },
+      { text: "#DontStopBelieving", type: "hashtag" },
+      { text: "#OopsIDidItAgain", type: "hashtag" },
+      { text: "Bulbasaur", type: "topic" },
+      { text: "Persona 5", type: "topic" },
+      { text: "Monster Hunter: World", type: "topic" },
+    ];
+  }
 }
 
-export getTrending;
+export { TweetsService };
