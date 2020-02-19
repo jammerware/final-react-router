509c3c1c05993ff4ce6dfb8b1e02c0f7e9ccf230
Glitch (sim50-activity6) <none>
2020-02-19 04:15:43 +0000

    💑🌾 Checkpoint ./src/components/site-banner.js:10221759/272

--- src/components/site-banner.js
+++ src/components/site-banner.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-class SiteBanner extends Component {
-  render() {
+class SiteBanner extends Component { 
+  render() {let bannerText = "Decent Twitter" ;
    return (
       <header>
        <h1>Decent Twitter</h1>
