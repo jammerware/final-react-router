95334ac1b22b913174255c99bb7617b1ef12ba38
Glitch (sim50-activity6) <none>
2020-04-15 18:35:59 +0000

    👔🏇 Checkpoint ./public/index.html:10218203/476 ./src/index.js:10218203/46 ./package.json:10218203/1126 ./src/all_user.js:10218203/111

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 console.log("index!");
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
