a29a51ee270af69f5879d4674b3381826474579e
Glitch (sim50-activity6) <none>
2020-04-15 14:18:56 +0000

    🐛🙅 Checkpoint ./src/plaza.js:10221759/1162 ./src/all_user.js:10221759/1157

---
+++ src/all_user.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { SiteBanner } from './components/site-banner';
+import { NewTweet } from './components/new-tweet';
+import { ViewTweet } from './components/view-tweet';
+import { TweetsService } from './services/tweets-service';
+
+import './App.css';
+
+class all_user extends Component {
+  constructor(state) {
+    super(state);
+    
+    this.state = { tweets: [] };
+  }
+  
+  componentDidMount() {
+    this._loadTweets();
+  }
+  
+  render() {
+    const tweetElements = this.state.tweets.map(tweet => <ViewTweet key={tweet.id} tweet={tweet}>hi</ViewTweet>);
+    
+    return (
+      <div className="app">
+        <SiteBanner bannerText="The BEST Twitter" />
+        <section className="content">
+          <NewTweet onTweetPosted={this._loadTweets.bind(this)}></NewTweet>
+          <hr />
+          <div className="recent-tweets">
+            <h1>Recent tweets</h1>
+            {tweetElements}
+          </div>
+        </section>
+      </div>
+    );
+  }
+  
+  _loadTweets() {
+    const tweetsService = new TweetsService();
+    const tweets = tweetsService.getTweets();
+    this.setState({ tweets });
+  }
+}
+
+export default App;
