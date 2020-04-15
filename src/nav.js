6f4b10cdb3a28c090985206ad54cf3d1f9b64a74
Glitch (sim50-activity6) <none>
2020-04-15 16:14:58 +0000

    👬💆 Checkpoint ./package.json:10221759/1059 ./src/App.css:10221759/1 ./src/nav.js:10221759/883

--- src/nav.js
+++ src/nav.js
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function nav(){
   return(
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
     
-     <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <link class="navbar-brand" href="/index.html">Website Name</a>
-        </div>
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="/signup.html"
-              ><span class="glyphicon glyphicon-user"></span> Sign Up</a
-            >
-          </li>
-          <li>
-            <a href="/login.html"
-              ><span class="glyphicon glyphicon-log-in"></span> Login</a
-            >
-          </li>
-        </ul>
-      </div>
-    </nav>
   );
 }
 
